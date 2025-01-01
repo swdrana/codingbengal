bf0d1c7642f834dc7ad71b3de1305e5a757137f6
swdrana <tctr2s@gmail.com>
2025-01-02 00:13:53 +0800

    about page updated

--- src/components/pages/about/AboutUs.js
+++ src/components/pages/about/AboutUs.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AboutUs = () => {
     return (
         <div>
+            <Link to={'https://blog.hubspot.com/marketing/remarkable-about-us-page-examples'}>get idea about about page</Link>
             <img src="https://lh5.googleusercontent.com/87EPFNH8qWlTqOLzHcWNPis-9eUQOjGheB4PrybBPYopPR6G5-M7Jzf-x-Ez94YnlaKPTJo5aM5RS54H893mvd_5gmmtzYkOwnkrtFt9VzYnJEAbnV0VXSQVpXkuvXPy2l9Y4PLC" alt="" />
         </div>
     );
